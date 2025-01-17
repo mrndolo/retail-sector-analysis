5e7bd33dbc11b6a1fc13ffb6741103e63e49d59d
mrndolo <callistomutunga@gmail.com>
2025-01-16 22:47:46 -0500

    handled date columns

--- retail-sector-analysis-with-r.R
+++ retail-sector-analysis-with-r.R
@@ -11,3 +11,11 @@ summary(data)
 
 # Check structure of the dataset
 str(data)
+
+# Convert DtCustomer to Date format
+data$DtCustomer <- as.Date(data$DtCustomer, format = "%Y-%m-%d")
+
+# Add a column for the number of years as a customer
+data$YearsAsCustomer <- as.numeric(difftime(Sys.Date(), data$DtCustomer, units = "weeks")) / 52
+
+
